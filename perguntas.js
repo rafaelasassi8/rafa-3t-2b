014ea7f53f0513635cc525208f19a607f5213c0b
rafaelasassi8 <143088302+rafaelasassi8@users.noreply.github.com>
2024-11-22 16:21:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'O último dragão',
+    'Onde passa a série?',
+    'Cidade do México'
+)
+
+criaCartao(
+    'O último dragão',
+    'Quantos episódios tem?',
+    '82'
+)
+
+criaCartao(
+    'O último dragão',
+    'Qual o nome dos irmãos de Miguel?',
+    'Jorge Chisca'
+)
+
+criaCartao(
+    'O último dragão',
+    'Onde Miguel morava?',
+    'Tóquio, Japão'
+)
+criaCartao(
+    'O último dragão',
+    'Como Adela e Miguel se conhecerem?',
+    'Adela quase atropela Miguel'
+)
+
+criaCartao(
+    'O último dragão',
+    'Quem dá a ordem pra matar os pais de Miguel?',
+    'Seu avô'
+)
+
